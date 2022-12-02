3ddd802c3a3d6deaf2c5a06522099912c3babb3d
BrandonWerbel <brandon@werbel.org>
2022-12-02 13:02:53 -0500

    Day 1

---
+++ src/Day1.hs
@@ -0,0 +1,31 @@
+module Day1 (someFunc) where
+
+import Data.List
+
+someFunc :: IO ()
+someFunc = do
+    contents <- readFile "input.txt"
+    let listNums = (map toInt (lines contents)) ++ [0]
+    let val = (addFunc listNums 0 0 [])
+    let cals = sort val
+    let total = (cals!!(length cals - 1)) + (cals!!(length cals - 2)) + (cals!!(length cals - 3))
+    print total
+        
+
+toInt str = 
+    if str == ""
+        then 0
+    else
+        read str :: Integer
+
+slice :: Int -> Int -> [a] -> [a]
+slice from to xs = take (to - from + 1) (drop from xs)
+
+addFunc list val i vals =
+    if list!!i == 0 then
+        if length list < 17 then
+            vals
+        else
+            addFunc (slice (i+1) (length list) list) 0 0 (vals++[val])
+    else
+        addFunc list (val + list!!i) (i+1) vals
