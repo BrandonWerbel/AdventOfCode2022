bbafa4114f3c4fa062b6f20b040887e08c645a66
BrandonWerbel <brandon@werbel.org>
2022-12-05 08:57:10 -0500

    Day 3 Part 2

--- src/Day3.hs
+++ src/Day3.hs
@@ -6,17 +6,25 @@ import Data.Maybe (mapMaybe)
 import Data.Char (isLetter, isUpper)
 import Data.List (find, intersect)
 
-splitAndFindFirstLetter :: String -> Maybe Int
-splitAndFindFirstLetter s = do
-    let n = length s
-    let (firstHalf, secondHalf) = splitAt (ceiling (fromIntegral n / 2)) s
-    let matchingLetters = intersect firstHalf secondHalf
-    let letter = head matchingLetters
-    let number = fromEnum letter - (if isUpper letter then 38 else 96)
-    return number
+splitAndFindFirstLetter :: [String] -> Maybe Int
+splitAndFindFirstLetter ss = do
+    let n = length ss
+    let firstThird = unwords (take (ceiling (fromIntegral n / 3)) ss)
+    let secondThird = unwords (take (ceiling (fromIntegral n / 3)) (drop (ceiling (fromIntegral n / 3)) ss))
+    let thirdThird = unwords (drop (ceiling (fromIntegral (n * 2) / 3)) ss)
+    let maybeLetter = find (\c -> c `elem` firstThird && c `elem` secondThird) thirdThird
+    case maybeLetter of
+      Just letter -> do
+        let number = fromEnum letter - (if isUpper letter then 38 else 96)
+        return number
+      Nothing -> Nothing
 
 someFunc :: IO ()
 someFunc = do
     input <- lines <$> readFile "input3.txt"
-    let total = sum (mapMaybe splitAndFindFirstLetter input)
+    let total = sum (mapMaybe splitAndFindFirstLetter (chunksOf3 input))
     print total
+
+chunksOf3 :: [a] -> [[a]]
+chunksOf3 [] = []
+chunksOf3 xs = take 3 xs : chunksOf3 (drop 3 xs)
